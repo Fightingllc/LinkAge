197b420874e17fec00249f7c9e5006df1768a3cb
刘凌晨 <liulingchen1109@163.com>
2022-08-18 01:41:43 +0800

    完成

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+/*
+ * @Author: 刘凌晨 liulingchen1109@163.com
+ * @Date: 2022-08-17 21:42:29
+ * @LastEditTime: 2022-08-17 22:13:05
+ * @FilePath: \demo\react-demo\src\App.tsx
+ */
+import { useState } from 'react'
+import LinkAge from './pages/LinkAge'
+
+// import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <LinkAge />
+    </div>
+  )
+}
+
+export default App
